0afecdc5427d54d836111ce1ed6abf5116d1f4b5
aliramirez08 <alicia.ramirez1@csuglobal.edu>
2025-02-17 01:09:50 -0500

    sdf

--- convert.py
+++ convert.py
@@ -1,6 +1,10 @@
 import os
 import yaml
 import eyed3
+import time
+
+def format_duration(seconds):
+    return time.strftime('%H:%M:%S', time.gmtime(seconds))
 
 def get_audio_files():
     audio_files = []
@@ -10,10 +14,15 @@ def get_audio_files():
             comments = ''
             if audio_file.tag.comments:
                 comments = ' '.join(comment.text for comment in audio_file.tag.comments)
+            file_path = os.path.join('audio', file)
             audio_files.append({
                 'title': audio_file.tag.title,
-                'comments': comments
+                'description': comments,
+                'file': f'/audio/{file}',  # Prefixed filename
+                'duration': format_duration(audio_file.info.time_secs),  # Duration in HH:MM:SS format
+                'length': f"{os.path.getsize(file_path):,}"  # Size in bytes with commas
             })
     return audio_files
 
-print(yaml.dump(get_audio_files()))
+with open('episodes.yaml', 'w') as file:
+    yaml.dump(get_audio_files(), file, sort_keys=False)
